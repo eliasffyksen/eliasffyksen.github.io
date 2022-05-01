6ebbac98962b591bec05881868d10f0eecdb2239
Elias F. Fyksen <git@fyksen.net>
2022-05-02 00:12:10 +0100

    Test 404

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { navigate } from 'svelte-routing';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		 const urlSearchParams = new URLSearchParams(window.location.search);
+		 if (urlSearchParams.has('p')) {
+			 navigate(urlSearchParams.get('p'));
+		 }
+	});
+
+</script>
+
+<main>
+</main>
+
+<style>
+</style>
